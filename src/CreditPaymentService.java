f46bb6ada0f77a188242d5724a2df959f36d0cfa
AnnaAndreeva92 <ann-stesha@yandex.ru>
2022-05-07 20:52:36 +0300

    calculate

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,11 @@
+public class CreditPaymentService {
+
+    public double calculaste (double amount, double years) {
+        double months = years * 12;
+        double percent = amount * 9.99 / 100;
+        double payment = ( amount + percent ) / months;
+        System.out.println(("Ежемесячный платеж:") + payment);
+
+        return payment;
+    }
+}
