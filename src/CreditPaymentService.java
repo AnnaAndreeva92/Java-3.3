1cf34a63c3fea831b1213d075e1523f624dd1cd8
AnnaAndreeva92 <ann-stesha@yandex.ru>
2022-05-07 22:22:18 +0300

    calculate

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
 
-    public double calculaste (double amount, double years) {
+    public double calculaste(double amount, double years) {
         double months = years * 12;
         double percent = amount * 9.99 / 100;
-        double payment = ( amount + percent ) / months;
+        double payment = (amount + percent) / months;
         System.out.println(("Ежемесячный платеж:") + payment);
 
         return payment;
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService monthyPayment = new CreditPaymentService();
-      double playment = monthyPayment.calculaste(1_000_000,1);
+        double playment = monthyPayment.calculaste(1_000_000, 1);
         System.out.println();
 
-        double playment2 = monthyPayment.calculaste( 1_000_000, 2);
-                System.out.println();
+        double playment2 = monthyPayment.calculaste(1_000_000, 2);
+        System.out.println();
 
-                double plaayment3 = monthyPayment.calculaste(1_000_000, 3);
-                System.out.println();
+        double plaayment3 = monthyPayment.calculaste(1_000_000, 3);
+        System.out.println();
     }
 }
