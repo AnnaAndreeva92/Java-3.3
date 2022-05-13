ff463cc07242ba2490acb7e31d5d967a584ebf4a
AnnaAndreeva92 <ann-stesha@yandex.ru>
2022-05-13 23:12:15 +0300

    credit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,8 +2,11 @@ public class CreditPaymentService {
 
     public double calculaste(double amount, double years) {
         double months = years * 12;
-        double percent = amount * 9.99 / 100;
-        double payment = (amount + percent) / months;
+        double percent = 9.99 / (100 * 12);
+        double coefficient = 1 + percent;
+        double monthlyCommission = Math.pow(coefficient, months);
+        double payment = amount * (percent * monthlyCommission) / (monthlyCommission - 1);
+
         System.out.println(" Ежемесячный платеж:" + payment);
 
         return payment;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         CreditPaymentService monthyPayment = new CreditPaymentService();
         double playment = monthyPayment.calculaste(1_000_000, 1);
         System.out.println();
